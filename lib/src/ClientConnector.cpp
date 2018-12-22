73321a543b3205771ba5e1b73b5ac731fc65e749
lychanl <lycha_@poczta.onet.pl>
2018-12-22 20:43:11 +0100

    Connector base class

--- lib/src/ClientConnector.cpp
+++ lib/src/ClientConnector.cpp
@@ -12,12 +12,9 @@ using namespace simplefs;
 
 ClientConnector::ClientConnector()
 {
-	socketFD = socket(AF_UNIX, SOCK_STREAM, 0);
-	if (socketFD == -1)
-	{
-		ok = false;
+	int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (sock == -1)
 		return;
-	}
 
 	struct sockaddr_un addr;
 	memset(&addr, 0, sizeof(struct sockaddr_un));
@@ -25,71 +22,9 @@ ClientConnector::ClientConnector()
 	addr.sun_family = AF_UNIX;
 	strcpy(addr.sun_path, DAEMON_SOCKET_PATH);
 
-	if (connect(socketFD, (const sockaddr*)&addr, sizeof(struct sockaddr_un)) == -1)
-	{
-		ok = false;
+	if (connect(sock, (const sockaddr*)&addr, sizeof(struct sockaddr_un)) == -1)
 		return;
-	}
 	
-	ok = true;
-}
-
-ClientConnector::~ClientConnector()
-{
-	if (socketFD >= 0)
-		close(socketFD);
-}
-
-int ClientConnector::send(Packet& packet)
-{
-	int size = packet.getTotalLength();
-	char* buffer = new char[size];
-
-	packet.serialize(buffer);
-
-	if (write(socketFD, buffer, size) == -1)
-	{
-		delete[] buffer;
-		return -1;
-	}
-	
-	delete[] buffer;
-	return 0;
-}
-
-Packet* ClientConnector::receive()
-{
-	unsigned int id;
-	if (read(socketFD, &id, sizeof(unsigned int)) == -1)
-		return nullptr;
-
-	Packet* received = nullptr;
-	if (id == OKResponse::ID)
-		received = new OKResponse;
-	else if (id == ErrorResponse::ID)
-		received = new ErrorResponse;
-	else if (id == ShmemPtrResponse::ID)
-		received = new ShmemPtrResponse;
-
-	if (received == nullptr)
-		return nullptr;
-
-	read(socketFD, basicBuffer, received->getBaseLength());
-	if (received->getRemainderLength() == 0)
-		return received;
-
-	char* buf = received->getRemainderBuffer();
-	if (read(socketFD, buf, received->getRemainderLength()) == -1)
-	{
-		delete received;
-		return nullptr;
-	}
-
-	return received;
-}
-
-bool ClientConnector::isOk()
-{
-	return ok;
+	setSocket(sock);
 }
 
--- lib/src/ClientConnector.h
+++ lib/src/ClientConnector.h
@@ -1,30 +1,14 @@
 #pragma once
 
-#include "utils/src/IPCPackets.h"
+#include "utils/src/Connector.h"
 
 
 namespace simplefs
 {
-	class ClientConnector
+	class ClientConnector : Connector
 	{
 	public:
 		ClientConnector();
-		~ClientConnector();
-		
-		int send(Packet& packet);
-
-		/*
-		 * the new packet will be placed on stack,
-		 * it will be responiblity of caller to release memory
-		 */
-		Packet* receive();
-
-		bool isOk();
-	
-	private:
-		char basicBuffer[4 * sizeof(int)]; //enough for any packet except those with varying length
-		int socketFD;
-		bool ok;
 	};
 }
 
